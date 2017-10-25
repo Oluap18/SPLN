38086c21f06b545f8fb759ec0a872279c38bdf83
Paulo Guedes <paulojorge.guedes@hotmail.com>
2017-10-25 17:03:56 +0100

    Relações gerais feitas, adicionado um exemplo mais abrangente

--- TP1/familyTree-SPLN.pl
+++ TP1/familyTree-SPLN.pl
@@ -4,46 +4,47 @@ use strict;
 use warnings;
 use utf8::all;
 
+my @pessoas;
+
 
 my $PM = qr{[A-ZÁÀÃÉÚÍÓÇ][a-záàãéúíóç]+};
 my $de = qr{d[aoe]s?};
 my $s = qr{[\n ]};
 my $np = qr{$PM ($s $PM|$s $de $s $PM)*}x;            #nome proprio completo
 my $par = qr{pai|filh[oa]|sobrinh[oa]|net[oa]|irmão?|av[óô]|mae|ti[oa]|bisav[óô]|amig[oa]|prim[oa]|cunhad[oa]|sogr[oa]|nora|genro|amante|esposa|marido|padrasto|madrasta|bastardo};                              #relação de parentesco
-my $all = qr{\s[\s\w]*};
-my $pal = qr{\s[\w]+\s};
-my $all2 = qr{\s($pal|\s){0,10}};
+my $pal = qr{\w+};
+my $all = qr{(([^\n]$pal)|\.)(\W$pal?)*};
+my $allP = qr{([^\n]$pal)(\W$pal?)*?};
 
-
-while(<>) {
-    s/(^\s?|[.!?":]([ \n]|$PM)|^-- )/$1_/g;
-    while(/(\b$np)($all) ($par)($all)($np)/g) {
-      my $fPers = $1;                                 #first person
-      my $par2 = $4;                                   #relation
-      my $sPers = $6;                                 #second person
-      my $ver = $3;
-      if ($ver =~ /$all ($par) $all ($np) $all/x) {
-        my $temp = $1;
-        my $temp2 = $2;
-        print "$fPers -> $temp -> $temp2\n";
-        $fPers = $temp2;
-      } else {
-        if ($ver =~ /($np)/) {                                 #variável para verificação
-          $fPers = $1;
-        }
-      }
-      print "$fPers -> $par2 -> $sPers\n";
-    }
-    
-    s/_//g;
-}
 
 while(<>){
-  print("Bananas\n");
   s/(^\s?|[.!?":]([ \n]|$PM)|^-- )/$1_/g;
-  while(/(\b$np)($all2)($np)/g){
-    my $Pers1 = $1;
-    my $Pers2 = $4;
-    print "$Pers1 relaciona-se $Pers2\n";
+  while(/(\b$np)($all)($np)/g){
+    my $fPers = $1;
+    my $sPers = $7;
+    my $tudo = $3;
+    my $tPers = "";
+    my $temp = "";
+    my $count = 0;
+    $pessoas[$count++] = $fPers;  #guardar o primeiro nome no array
+    $pessoas[$count++] = $sPers;  #guardar o ultimo nome no array
+    print("$fPers relaciona-se com: $sPers\n");
+
+    while($tudo =~ /($allP) ($np)($all)/){
+      my $pessoa = $4;
+
+      foreach $a (keys @pessoas){
+        print("$pessoas[$a] relaciona-se com: $pessoa\n");
+      }
+
+      $pessoas[$count++] = $4;    #guardar os nomes próprios
+      $tudo = $6;         #iterar o ciclo
+
+
+      
+    }
+
+    #print("$fPers relaciona-se com $sPers\n");
   }
+  s/_//g;
 }
