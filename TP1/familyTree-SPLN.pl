25e47df754e794b06c731c574a911c3968cbe222
Paulo Guedes <paulojorge.guedes@hotmail.com>
2017-11-01 18:41:07 +0000

    Relações normais a dar, de parentesco a dar mal

--- TP1/familyTree-SPLN.pl
+++ TP1/familyTree-SPLN.pl
@@ -6,7 +6,7 @@ use warnings;
 use utf8::all;
 
 my $graph = Graph::Easy->new();
-my $i = 25;
+my $i = 1000000;
 my %sortedHash;
 my %sortedHashP;
 my %verify;
@@ -25,8 +25,9 @@ my $iterator;
 
 my $PM = qr{[A-ZÁÀÃÉÚÍÓÇ][a-záàãéúíóç]+};                                                   #palavra maiúscula
 my $de = qr{d[aoe]s?};                                                                      #conector - ex: "de,da,do"
-my $s = qr{[\n ]};                                                                          #space or new line
-my $np = qr{$PM ($s $PM|$s $de $s $PM)*}x;                                                  #nome proprio completo
+my $s = qr{[\n ]}; 
+my $Pre = qr{Sr\.|Sra\.|Dr\.|Dra\.|Eng\.|Miss\.|Mr\.};                                                                         #space or new line
+my $np = qr{$Pre? $PM ($s $PM|$s $de $s $PM)*}x;                                                  #nome proprio completo
 my $rSupM = qr{genro|amante|marido|padrasto};                                               #relação "superior" -> masculino
 my $rSup = qr{pai|av[óô]|ti[oa]|bisav[óô]|amig[oa]|cunhad[oa]|sogr[oa]};                    #relação "superior"
 my $rSupF = qr{mãe|nora|esposa|madrasta};                                                   #relação "superior" -> feminino
@@ -37,64 +38,82 @@ my $all = qr{(([^\n]$pal)|[.,;-?!«»:'"])(\W$pal?)*};
 my $allP = qr{([^\n]$pal)(\W$pal?)*?};
 
 while(<>){
-  s/(^[\s'"«]?|[.!?":;]([ \n]|$PM)|^-- )/$1_/g;
-  s/\b($np)/{$1}/g;
+  	s/(^|[\n]|[?!.;:]|['"«]|[-—]|^--) ?($PM)/$1_$2/g;
+  	s/($Pre)(_)($np)/_$1_$3/g;																#Tratar dos casos que existem prefixos
+  	s/(\b$np)/{$1}/g;
+  	s/(_)($Pre)(_)($np)/{$2 $4}/g;															#Tratar dos casos que existem prefixos
+  	print $_;
 
-  while(/{($np)}($all){($np)}/g){
-    my @pessoas;
-    $fPers = $1;                                                                            #guarda o primeiro nome do paragrafo
-    $sPers = $7;                                                                            #guarda o último nome do paragrafo
-    $tudo = $3;                                                                             #guarda todo o conteudo entre os nomes próprios
-    $count = 0;
-    $pessoas[$count++] = $fPers;  
-    if($tudo =~ /{($np)}/){
-      while($tudo =~ /($allP) \{($np)\}($all)/){                                               #enquanto enquanto houver nomes próprios entre os dois nomes próprios do maior regex
-        $pessoa = $4;
-        $countAux = 0;
-        foreach $a (keys @pessoas){
-          if($1 =~ /($par)/){                                                               #se tiver relações de parentesco
-            if($countAux == $count-1){                                                      #se a relação de parentesco pertencer ao nome próprio no indice do array
-              verifica_relacao($pessoas[$a],$pessoa,$1);
-            }
-            else{
-              verifica($pessoas[$a], $pessoa);
-            }
-          }
-          else{
-            verifica($pessoas[$a],$pessoa);
-          }
-          $countAux++;
-        }
-        $pessoas[$count++] = $4;                                                            #guardar os nomes próprios
-        $tudo = $6;                                                                         #iterar o ciclo
-      }
-      my $countAux = 0;                                                                     #Estabelecer a relação entre os nomes do array e o último nome próprio
-      foreach $a (keys @pessoas){
-        if($tudo =~ /($par)/){                                                              #Caso exista uma relação de parentesco entre o último e penúltimo nome próprio.                                        #$tudo neste caso é a relação entre o penúltimo e último nome próprio
-          if($countAux == $count-1){                                                        #se a relação de parentesco pertencer ao nome próprio no indice do array
-              verifica_relacao($pessoas[$a],$sPers,$1);
-          }
-          else{
-              verifica($pessoas[$a], $sPers);
-          }
-        }
-        else{
-          verifica($pessoas[$a], $sPers);
-        }
-        $countAux++;
-      }
-    }
-    else{
-      if($tudo =~ /($par)/){                                                                #se tiver 2 nomes, e tiverem parentesco
-        verifica_relacao($fPers,$sPers,$1);;
-      }
-      else{
-        verifica($fPers, $sPers);
-      }
-    }
-  }
-  s/{($np)}/$1/g;
-  s/_//g;
+  	while(/{($np)}($all){($np)}/g){
+	    my %pessoas;
+	    $fPers = $1;                                                                            #guarda o primeiro nome do paragrafo
+	    $sPers = $7;                                                                            #guarda o último nome do paragrafo
+	    $tudo = $3;                                                                             #guarda todo o conteudo entre os nomes próprios
+	    $count = 0;
+	    $pessoas{$fPers}++;
+	    $count++; 
+	    my $pessoaAux = $fPers;
+
+	    if($tudo =~ /{($np)}/){
+	      	while($tudo =~ /($allP) \{($np)\}($all)/){
+		        $pessoa = $4;
+		        $countAux = 0;
+		        foreach my $key (keys %pessoas){
+			        if($1 =~ /($par)/){     	
+			          	if($countAux == $count-1){  
+			           		verifica_relacao($pessoaAux,$pessoa,$1);
+			           	}
+			          	else{
+			           		if(!exists $pessoas{$pessoa}){
+				           		verifica($key, $pessoa);
+				           	}
+				        }
+			      	}
+			      	else{
+			      		if(!exists $pessoas{$pessoa}){
+				           	verifica($key,$pessoa);
+			    	    }
+			    	}
+			    	$countAux++;  
+			   	}
+			    if(!exists $pessoas{$pessoa}){
+			      	$pessoas{$pessoa}++; 
+			       	$count++; 			     
+			    }                                                   #guardar os nomes próprios
+			    $tudo = $6;
+			    $pessoaAux = $4;                                                                         #iterar o ciclo
+		    }
+		    $countAux = 0;                                                                     #Estabelecer a relação entre os nomes do array e o último nome próprio
+		    foreach my $key (keys %pessoas){
+		      	if($tudo =~ /($par)/){                                                              #Caso exista uma relação de parentesco entre o último e penúltimo nome próprio.                                        #$tudo neste caso é a relação entre o penúltimo e último nome próprio
+		       		if($countAux == $count-1){   
+		           		verifica_relacao($pessoaAux,$sPers,$1);
+		       		}
+		       		else{
+		       			if(!exists $pessoas{$sPers}){
+		           			verifica($key, $sPers);
+		           		}
+		       		}
+		      	}
+		      	else{
+		       		if(!exists $pessoas{$sPers}){
+		           		verifica($key, $sPers);
+		           	}
+		       	}
+		      	$countAux++;
+		    }
+		}
+		else{
+		   	if($tudo =~ /($par)/){                                                                #se tiver 2 nomes, e tiverem parentesco
+		   		verifica_relacao($fPers,$sPers,$1);;
+		   	}
+		   	else{
+		      	verifica($fPers, $sPers);
+		   	}
+	    }
+	}
+	s/{($np)}/$1/g;
+	s/_//g;
 }
 
 print "As relações de parentesco são: \n";
@@ -102,6 +121,7 @@ print "As relações de parentesco são: \n";
 for (sort{$sortedHashP{$b} <=> $sortedHashP{$a}} keys %sortedHashP){
     if(/($np)-($np)-($par)/g) {
        $union = "$1-$3-$5";
+       print("$1 -> $3 -> $5\n");
        
     }
     $i--;
