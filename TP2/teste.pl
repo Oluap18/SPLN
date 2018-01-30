9da5ff20d84d3b05fcafa63770ad6dea9628fd0f
Pedro Cunha <pedrocunha1596@gmail.com>
2018-01-30 15:00:15 +0000

    sub weather adicionada ao teste.pl

--- TP2/teste.pl
+++ TP2/teste.pl
@@ -14,11 +14,11 @@ my $topico; #Modo do bot. $1 = Turismo. $0 = Regras.
 my $PM = qr{[A-ZÁÀÃÉÊÚÍÓÇ]|[a-záàãéêúíóç]+};
 my $de = qr{d[aoe]s?};
 my $s = qr{[\n ]};
-my $np = qr{$PM ($s $PM| $PM)*}x;	
+my $np = qr{$PM ($s $PM| $PM)*}x;
 
-#Guardar as regras. 
+#Guardar as regras.
 #Regras começadas por R: são regras.
-#Regras começadas por A: são respostas. 
+#Regras começadas por A: são respostas.
 while(<>){
 
 	#Guardar as regras
@@ -103,7 +103,7 @@ while(<STDIN>){
 			$indice = $_;
 			turismo(\%arrayQuest, $indice);
 			$topico = "1";	#Resposta dada pela vertente do turismo do bot.
-			last;					
+			last;
 		}
 	}
 
@@ -192,4 +192,49 @@ sub turismo{
 					$results = $2;
 				}
 			}
+}
+
+#palavras-chave - tempo, meteorologia, temperatura, temperaturas, condições meteorológicas
+# recebe como argumento o nome da cidade, em minusculas
+sub weather{
+  my @day;
+  my @temp;
+  my $city = shift;
+  my $counter = 1;
+  my $ua = new LWP::UserAgent;
+  $ua->timeout(120);
+  my $url="https://www.tempo.pt/$city.htm";
+  my $request = new HTTP::Request('GET', $url);
+  my $response = $ua->request($request);
+  my $content = $response->content();
+  open my $fh, "<", \$content;
+  while(my $row = <$fh>) {
+    while($row =~ /datetime="([0-9]{4})-([0-9]{2})-([0-9]{2})"/g) {
+      push(@day,"$3-$2-$1");
+    }
+  }
+  close $fh;
+  open my $fh, "<", \$content;
+  my $t;
+  while(<$fh>) {
+    while(/ (-?[0-9]+)&deg; /g) {
+      my $info;
+      if (($counter % 2) eq 0) {
+        $info = "Temperatura Máxima: $t ºC \n Temperatura minima: $1 ºC\n";
+        push(@temp, $info);
+        undef $info;my $counter = 1;
+      }
+      else {
+      }
+      $t = $1;
+      $counter++;
+    }
+  }
+  print "Qual o intervalo de tempo que desejas? (1 a 7 dias): ";
+  my $time = <STDIN>;
+  for my $i (0 .. ($time-1)) {
+    my $first  = $day[$i];
+    my $second = $temp[$i];
+    print "$first \n $second \n"
+  }
 }
