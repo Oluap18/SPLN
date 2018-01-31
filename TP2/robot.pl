9bfc95a09863ddecbd49dfd46a4fbb555fa2f965
Paulo Guedes <paulojorge.guedes@hotmail.com>
2018-01-31 19:38:29 +0000

    Adicionadas funcionalidades de meteorologia, noticias e informações gerais. Corrigido bug das regras

--- TP2/robot.pl
+++ TP2/robot.pl
@@ -1,7 +1,9 @@
 use strict;
 use threads;
 use threads::shared;
+use LWP::UserAgent;
 use utf8::all;
+use Unicode::Normalize;
 
 my %rulesAux; #O que suporta as regras iniciais
 my %rules:shared; #O que vai suportar as regras aquando aplicada a lemmatização
@@ -10,8 +12,12 @@ my @threads;
 my $rule;
 my $answer;
 my $input;
+my $resposta; #Se o bot já respondeu ou não.
 
-my $pal = qr{[\wáàãéêúíóç]+};	
+my $PM = qr{[A-ZÁÀÃÉÊÚÍÓÕÇ]|[a-záàãéêúíóõç]+};
+my $de = qr{d[aoe]s?};
+my $s = qr{[\n ]};
+my $np = qr{$PM ($s $PM| $PM)*}x;	
 
 #Guardar as regras. 
 #Regras começadas por R: são regras.
@@ -29,18 +35,34 @@ while(<>){
 my $thread;
 #Tokanizar e lemmatizar as regras
 for my $key(keys %rulesAux){
+	my $aux = $key;
+	my $counter = 0;
 	my $string;
+	my @raw;
 	push @threads, async{
+		while($aux =~ /(["',.!?()+*]|($PM+)[ -]?)(.*)/){
+			$raw[$counter++]= $1;
+			$aux = $3;
+		}
 		my @output = qx{echo '$key' | analyze -f /usr/local/share/freeling/config/pt.cfg};
+		$counter = 0;
 		for (@output){
 			if($_){ 		#para retirar possiveis \n que tenha
 				/.*? (.*?) .*/;
-				$string = join('', $string, "$1 ");
+				my $aux = $1;
+				if($raw[$counter] =~ /[A-Z]$PM*/){
+					$string = join('', $string, ucfirst($aux), " ");
+				}
+				else{
+					$string = join('', $string, "$aux ");
+				}
+				$counter++;
 			}
 		}
 		$rules{$string} = $rulesAux{$key};
 	}
 }
+
 
 for(@threads){
 	$_ -> join;
@@ -50,63 +72,176 @@ print "Ola, queres conversar comigo?\n";
 
 #Proceder à comparação de perguntas, às regras.
 while(<STDIN>){
+	$resposta = 0;
 	my $string;
+	my $aux = $_;
 	my $counter = 0;
 	my %arrayQuest;
+	my @raw;
 	#Lemmatizar e tokanizar a pergunta do utilizador
+	while($aux =~ /(["',.!?()+*]|($PM+) ?)(.*)/){
+		$raw[$counter++]= $1;
+		$aux = $3;
+	}
 	my @output = qx{echo '$_' | analyze -f /usr/local/share/freeling/config/pt.cfg};
+	$counter = 0;
+	chomp(@output);
 	for (@output){
-		if($_){ 		#para retirar possiveis \n que tenha
+		if($_){ #Chomp tira todos por alguma razão
 			/.*? (.*?) .*/;
-			$arrayQuest{$counter++} = $1;
+			my $aux = $1;
+			if($raw[$counter] =~ /[A-ZÁÀÃÉÊÚÍÓÕÇ]\w*/){
+				$arrayQuest{$counter++} = ucfirst($aux);
+			}
+			else{
+				$arrayQuest{$counter++} = $aux;
+			}
 		}
 	}
-	my $valorComp = 0;
-	my $keyOri;
-	my $answer;
-	my $nPal = 0;
-	#Comparar a pergunta às rules
-	for my $key(keys %rules){
-		$keyOri = $key;
-		my %arrayRule;
-		my $counterR = 0;
-		my $valorCompAux = 0;
-		my $counterQ = 0;
-		my $valorBonus = 0;			#Valor de comparação bónus por ter palavras seguidas iguais
 
-		chomp $key;
+	for my $counter (keys %arrayQuest){
+		my $indice;
+		#Turismo
+		if($arrayQuest{$counter} =~ /(cidade|turismo|local|conhecer|visitar|informação)/){
+			$indice = $counter;
+			while(!($arrayQuest{++$indice} =~ /[A-ZÁÀÃÉÊÚÍÓÕÇ].*/)){}
+			if($indice != (scalar keys %arrayQuest)){
+				my $nome = $arrayQuest{$indice};
+				turismo($nome);
+				$resposta = "1";	
+			}			
+		}
+		#Meteorologia
+		if($arrayQuest{$counter} =~ /(tempo|meteorologia|temperatura|temperaturas|meteorológico|meteorologia|informação)/){
+			$indice = $counter;
+			while(!($arrayQuest{++$indice} =~ /[A-ZÁÀÃÉÊÚÍÓÕÇ].*/)){}
+			if($indice != (scalar keys %arrayQuest)){
+				my $nome = $arrayQuest{$indice};
+				#Retirar acentos
+				$nome = NFKD($nome);
+				$nome =~ s/\p{NonspacingMark}//g;
+				#Colocar as palavras a minúsculo
+				$nome =~ y/[A-Z]_/[a-z]-/;
+				weather($nome);
+				$resposta = "1";	
+			}			
+		}
+		#Biografia
+		if($arrayQuest{$counter} =~ /(informação|quem|biografia)/){
+			$indice = $counter;
+			#Palavra chave : Quem é
+			if($arrayQuest{$indice} =~ /quem/){
+				for my $key(keys %arrayQuest){
+					if($arrayQuest{$key} =~ /ser/ & $key > $indice){
+						$indice = $key;
+						while(!($arrayQuest{++$indice} =~ /[A-ZÁÀÃÉÊÚÍÓÕÇ].*/)){}
+						if($indice != (scalar keys %arrayQuest)){
+							my $nome = $arrayQuest{$indice};
+							my $nomeCaps;			#Nome com as letras iniciais maiúscilas
+							while($nome =~ /(.*?)_(.*)/){
+								$nome = $2;
+								$nomeCaps = join('', $nomeCaps, ucfirst($1), "_");
+							}
+							$nomeCaps = join('', $nomeCaps, ucfirst($nome));
+							personInfo($nomeCaps);
+							$resposta = "1";	
+						}
+					}
+				}
+			}
+			#Todas as outras
+			else{
+				while(!($arrayQuest{++$indice} =~ /[A-ZÁÀÃÉÊÚÍÓÕÇ].*/)){}
+				if($indice != (scalar keys %arrayQuest)){
+					my $nome = $arrayQuest{$indice};
+					my $nomeCaps;			#Nome com as letras iniciais maiúscilas
+					while($nome =~ /(.*?)_(.*)/){
+						$nome = $2;
+						$nomeCaps = join('', $nomeCaps, ucfirst($1), "_");
+					}
+					$nomeCaps = join('', $nomeCaps, ucfirst($nome));
+					personInfo($nomeCaps);
+					$resposta = "1";	
+				}
+			}
+		}
+		#Noticias
+		if($arrayQuest{$counter} =~ /(noticiar|informação)/){
+			$indice = $counter;
+			while(!($arrayQuest{++$indice} =~ /[A-ZÁÀÃÉÊÚÍÓÕÇ].*/)){}
+			my $nome;
+			if($indice != (scalar keys %arrayQuest)){
+				$nome = $arrayQuest{$indice};
+				$nome =~ y/[A-ZÁÀÃÉÊÚÍÓÕÇ]_/[a-zaaaeeuiooç]-/;
+				noticias($nome);
+				$resposta = "1";
+			}
+			else{
+				$nome = "Mundo";
+				noticias($nome);
+				$resposta = "1";
+			}	
+		}
+		last if($resposta == "1");
+
+	}
+	if($resposta == "0"){
+		regras(\%rules, \%arrayQuest);
+	}
+		
+}
+
+sub regras{
+	my ($rules, $arrayQuest) = @_;
+
+	my $valorComp = 0;	#Valor de comparação da string
+	my $keyOri;			#Chave original, sem ser alterada
+	my $answer;			#Resposta a ser dada pelo bot
+	my $nPal=0;
+	#Comparar a pergunta às rules
+	for my $key(keys %$rules){
+		$keyOri = $key;
+		my %arrayRule;		#Array que vai conter as palavras tokenizadas da regra a comparar
+		my $counterR = 0;	#Contador para percorrer as regras
+		my $valorCompAux = 0;		#Valor de comparação auxiliar que vai servir para comparar com o valor de comparaçao das outras regras
+		my $counterQ = 0;			#Contador que vai percorrer a pergunta
+		my %arrayI;					#Posição das palavras nas regras (-3 caso não existam), para adicionar bónus de palavras seguidas, e para ter em conta palavras repetidas
+
 		#Segmenting/tokenizing a Rule
 		while($key =~ /(.*?) (.*)/){
-			$arrayRule{$counterR++} = $1;
+			$arrayRule{$counterR} = $1;
+			$arrayRule{$counterR++} =~ y/[A-Z]/[a-z]/;
 			$key = $2;
 		}
+
 		#Percorrer todas as palavras da pergunta
-		while($counterQ != (scalar keys %arrayQuest) - 1){
+		while($counterQ != scalar keys %$arrayQuest){
+			$arrayQuest->{$counterQ} =~ y/[A-Z]/[a-z]/;
 			$counterR = 0;
 			#Percorrer todas as palavras da rule, até encontrar uma palavra igual à da pergunta
-			while(!($arrayQuest{$counterQ} =~ /\Q$arrayRule{$counterR}\E/)){
+			while($arrayQuest->{$counterQ} cmp $arrayRule{$counterR} || exists $arrayI{$counterR}){
+				my $teste = $arrayQuest->{$counterQ} cmp $arrayRule{$counterR};
+				last if($counterR == scalar keys %arrayRule);
 				$counterR++;
 			}
 			#Se encontrou uma palavra igual
-			if($counterR != (scalar keys %arrayRule) -1){
-				print "$arrayRule{$counterR}\n";
-				#Somar o valor de comparação bónus das palavras seguidas, mais o facto de a palavra ser igual
-				$valorCompAux+=$valorBonus+1;
-				$valorBonus++;
-			}
-			#Se não encontrou
-			else{
-				$valorBonus--;
+			if($counterR != (scalar keys %arrayRule)){
+				$arrayI{$counterR} = $counterQ;
+				my $c1 = $counterQ-1;
+				my $c2 = $counterQ-2;
+				#Se alguma das 2 palavras anteriores for igual na regra e na pergunta
+				$valorCompAux+=2 if($arrayI{$counterR-1} =~ /($c1|$c2)/ || $arrayI{$counterR-2} =~ /($c1|$c2)/);
+				$valorCompAux+=1;
 			}
 			#Proxima palavra
 			$counterQ++;
 		}
-		my $teste = (scalar keys %arrayRule) -1;
+
 		#Se tiver mais pontos de comparação.
 		if($valorCompAux > $valorComp){
 			#Guarda o valor de comparação, a resposta e o número de palavras da rule
 			$valorComp = $valorCompAux;
-			$answer = $rules{$keyOri};
+			$answer = $rules->{$keyOri};
 			$nPal = (scalar keys %arrayRule) -1;
 		}
 		#Se tiver os mesmos pontos de comparação, é escolhido
@@ -116,13 +251,14 @@ while(<STDIN>){
 			if((scalar keys %arrayRule) - 1 < $nPal){
 				#Guarda o valor de comparação, a resposta e o número de palavras da rule
 				$valorComp = $valorCompAux;
-				$answer = $rules{$keyOri};
+				$answer = $rules->{$keyOri};
 				$nPal = (scalar keys %arrayRule) -1;
 			}
 		}
 	}
+	print "Valor: $valorComp\n";
 	my $line;
-	if($valorComp > 0) {
+	if($valorComp > 0){
 		print "$answer\n";
 	}
 	else{
@@ -132,4 +268,148 @@ while(<STDIN>){
 		close FILE;
 		print "$line";
 	}
+}
+
+sub turismo{
+	my @prefixos = ("Ah, ", "");
+	my @sufixos = (" tem muitos locais túristicos que podes visitar.\n", " belo sitio para passar férias.\n", " podes passar um bom tempo lá.\n");
+	my @locais;
+	my ($nome) = @_;
+	
+	my $url = join('', "http://www.google.com/search?hl=pt&q=", $nome, "_Principais_Locais&ie=UTF-8");
+	my $results = qx(curl -sA "Chrome" -L '$url' | iconv -f iso8859-1 -t utf-8);
+	while($results =~ /,<br>(.*?)<\/span>(.*)/){
+		push @locais, $1;
+		$results = $2;
+	}
+
+	my $rand = int(rand(scalar @prefixos -1));
+	print $prefixos[$rand];
+	while($nome =~ /(.*?)_(.*)/){
+		print ucfirst("$1 ");
+		$nome = $2;
+	}
+	print ucfirst("$nome,");
+	$rand = int(rand(scalar @sufixos -1));
+	print $sufixos[$rand];
+
+	if(scalar @locais != 0){
+		print "Aqui estão alguns locais que podes visitar:\n";
+		my $counter = 0;
+		while($counter < 10 & $counter != scalar @locais){
+			print "$locais[$counter++]\n";
+		}
+		print "Deseja conhecer mais locais?\n";
+		$resposta = <STDIN>;
+		$resposta =~ y/[A-Z]/[a-z]/;
+		if($resposta =~ /sim/){
+			while($counter != scalar @locais){
+				print "$locais[$counter++]\n";
+			}
+			print "Estes são todos os locais turisticos que conheço.\n";
+		}
+		print "Muito bem. Que mais queres falar?\n";
+	}
+	else{
+		$resposta="0";
+	}
+}
+
+
+sub weather{
+	my @day;
+	my @temp;
+	my $city = shift;
+	my $counter = 1;
+	my $ua = new LWP::UserAgent;
+	$ua->timeout(120);
+	my $url="https://www.tempo.pt/$city.htm";
+	my $request = new HTTP::Request('GET', $url);
+	my $response = $ua->request($request);
+	my $content = $response->content();
+	open my $fh, "<", \$content;
+	while(my $row = <$fh>) {
+		while($row =~ /datetime="([0-9]{4})-([0-9]{2})-([0-9]{2})"/g) {
+	    	push(@day,"$3-$2-$1");
+	    }
+	}
+	close $fh;
+	open my $fh, "<", \$content;
+	my $t;
+	while(<$fh>) {
+	    while(/ (-?[0-9]+)&deg; /g) {
+		    my $info;
+	    	if (($counter % 2) eq 0) {
+	        	$info = "Temperatura Máxima: $t ºC \n Temperatura minima: $1 ºC\n";
+	        	push(@temp, $info);
+	        	undef $info;my $counter = 1;
+	      	}
+	      	$t = $1;
+	      	$counter++;
+	    }
+	}
+	if(scalar @temp != 0){
+		print "Qual o intervalo de tempo que desejas? (1 a 7 dias): ";
+		my $time = <STDIN>;
+		for my $i (0 .. ($time-1)) {
+		   	my $first  = $day[$i];
+		   	my $second = $temp[$i];
+		   	print "$first \n $second \n"
+		}
+	}
+	else{
+		$resposta="0";
+	}
+}
+
+sub personInfo{
+	my $name = shift;
+	my $info;
+	$_ = $name;
+	s/ +/_/g;
+	$name = $_;
+	my $ua = new LWP::UserAgent;
+	$ua->timeout(120);
+	my $url="https://pt.wikipedia.org/wiki/$name";
+	my $request = new HTTP::Request('GET', $url);
+	my $response = $ua->request($request);
+	my $content = $response->content();
+	open my $fh, "<", \$content;
+	while(my $row = <$fh>) {
+	    if ($row =~ /<p><b>(.+)\.(.+)?<\/p>/) {
+		    $info = $1;
+	    }
+	}
+	$_ = $info;
+	if ($info) {
+		s/<.*?>//g;
+	    s/\[[0-9]+\]//g;
+	    print "$name\n";
+	    $info = $_;
+	    print "\n$info. \n \n \n";
+	}
+	else {
+		$resposta="0";
+	 }
+}
+
+#Se não for especificada o assunto da noticia, a variavel search deve ficar a "mundo" | 4 noticias neste momento
+sub noticias{
+  	my $news;
+  	my $counter = 1;
+  	my $search = shift;
+  	my $url = join('', "http://www.google.pt/search?q=", $search, "+noticias&source=lnms&tbm=nws&sa=X&ved");
+  	my $results = qx(curl -sA "Chrome" -L '$url' | iconv -f iso8859-1 -t utf-8);
+  	while($results =~ /class="st">(.*?)\./g){
+    	$_ = $1;
+    	s/<.*?>//g;
+    	s/\[[0-9]+\]//g;
+    	print "Noticia $counter:\n";
+    	$news = $_;
+    	print "--> $news;\n\n";
+    	$counter++;
+    	if ($counter eq 5) {
+      	last;
+    	}
+  	}
 }
